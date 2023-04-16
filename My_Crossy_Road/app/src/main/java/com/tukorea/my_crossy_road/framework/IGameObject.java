8a2bc49cfacf9aac8b722979d67dcff5d34397c7
chs0218 <39898936+chs0218@users.noreply.github.com>
2023-04-16 17:19:53 +0900

    강의시간에 진행한 기본 프레임워크 패키지 가져오기

---
+++ My_Crossy_Road/app/src/main/java/com/tukorea/my_crossy_road/framework/IGameObject.java
@@ -0,0 +1,8 @@
+package com.tukorea.my_crossy_road.framework;
+
+import android.graphics.Canvas;
+
+public interface IGameObject {
+    public void update();
+    public void draw(Canvas canvas);
+}
